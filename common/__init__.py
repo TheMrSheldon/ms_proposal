2d060c980c897b56f8eb897ca9220dc7cb00cb62
Tim Hagen <timhagenmail@googlemail.com>
2023-01-23 11:03:23 +0100

    fixed an issue with caching sometimes not correctly computing the hash and added further logging

--- common/__init__.py
+++ common/__init__.py
@@ -1,11 +1,13 @@
 import os
+from typing import Optional
 
 
-def set_cuda_devices_env(devices: list[str]):
-    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    print(f"Set CUDA_DEVICE_ORDER to '{os.environ['CUDA_DEVICE_ORDER']}'")
-    os.environ["CUDA_VISIBLE_DEVICES"] = "-1" if not devices else ",".join(map(str, devices))
-    print(f"Set CUDA_VISIBLE_DEVICES to '{os.environ['CUDA_VISIBLE_DEVICES']}'")
+def set_cuda_devices_env(devices: Optional[list[str]]):
+    if devices is not None:
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        print(f"Set CUDA_DEVICE_ORDER to '{os.environ['CUDA_DEVICE_ORDER']}'")
+        os.environ["CUDA_VISIBLE_DEVICES"] = "-1" if not devices else ",".join(map(str, devices))
+        print(f"Set CUDA_VISIBLE_DEVICES to '{os.environ['CUDA_VISIBLE_DEVICES']}'")
 
 
 __all__ = [
--- proposal/proposal.py
+++ proposal/proposal.py
@@ -8,6 +8,7 @@ from ranking_utils.model.data import DataProcessor
 
 import torch
 from torch.nn import BCEWithLogitsLoss, MSELoss
+import torch.nn.functional as F
 from torch.linalg import vector_norm
 from torch_geometric.data import Batch as tgBatch, Data
 # from torch_geometric.utils.convert import to_scipy_sparse_matrix
@@ -111,15 +112,29 @@ class ProposedRanker(Ranker):
 
     @torch.no_grad()
     def _fw_colbert_single(self, input_ids: torch.Tensor, attention_mask: torch.Tensor) -> torch.Tensor:
-        key = tensor_hash(input_ids)
+        # We only use unmasked entries of the input_ids to compute a hash for the input. Otherwise we may run into the
+        # following issue:
+        # Let tokenized inputs be of different dimensions i.e. the following input_ids:
+        # id1: [ 101, 7592, 2088,  102]    and    id2: [ 101, 3231,  102]
+        # When batched id2 will be extended to [ 101, 3231,  102, 0] to match the dimensions of id1 but these 0's are
+        # masked in the attention masks:
+        # am1: [ 1, 1, 1, 1]    and    am2: [ 1, 1, 1, 0]
+        # In another batch, however, the same input as for id2 would have input_ids: [ 101, 3231,  102, 0, 0] with the
+        # last two entries masked out and a simple hash of the input_ids would not recognize it to be effectively the
+        # same input.
+        input_length = torch.nonzero(attention_mask)[-1]+1
+        unmasked = input_ids[:input_length]
+        key = tensor_hash(unmasked)
         cache_file = self.cache_dir / f"{key}"
         if cache_file.exists():
-            data = torch.load(cache_file).to(self.device)
+            data = torch.load(cache_file, map_location=self.device)
             assert isinstance(data, torch.Tensor)
         else:
-            input = {"input_ids": input_ids.unsqueeze(0), "attention_mask": attention_mask.unsqueeze(0)}
+            input = {"input_ids": unmasked.unsqueeze(0), "attention_mask": attention_mask[:input_length].unsqueeze(0)}
             data = self.colbert.forward_representation(input).squeeze()
             torch.save(data, cache_file)
+        data = F.pad(input=data, pad=(0, 0, 0, attention_mask.size(0)-input_length), mode='constant', value=0)
+        assert data.size(0) == input_ids.size(0)
         return data
 
     @torch.no_grad()
@@ -131,7 +146,7 @@ class ProposedRanker(Ranker):
         # Calculate the l2-norm for each graph's edge_weights
         edge_attrs = unbatch_edge_attr(doc_graphs.edge_weight, doc_graphs.edge_index, doc_graphs.batch)
         norms = [vector_norm(v, ord=2) for v in edge_attrs]
-        return torch.tensor(norms)
+        return torch.tensor(norms, device=self.device)
 
     def forward(self,
                 batch: Batch,
@@ -156,12 +171,18 @@ class ProposedRanker(Ranker):
         # teacher_pred = self.colbert.forward_aggregation(query_vecs, teacher_doc_vecs)
 
         distillation_loss = self.mseloss(doc_vecs, teacher_doc_vecs)
-        sparsity = self._sparsity(doc_graphs).to(self.device)
+        sparsity = self._sparsity(doc_graphs)
         classification_loss = self.bce(pred.flatten(), labels.flatten())
 
         # Ablation: different combinations of these lossfunctions and their effect on training
         loss = torch.mean(distillation_loss + sparsity + classification_loss)
-        self.log("train_loss", loss)
+        # loss = torch.mean(distillation_loss + classification_loss)
+        self.log_dict({
+            "loss/total": loss,
+            "loss/distillation": torch.mean(distillation_loss),
+            "loss/sparsity": torch.mean(sparsity),
+            "loss/classification": torch.mean(classification_loss)
+        })
         return loss
 
     def configure_optimizers(self) -> tuple[list[Any], list[Any]]:
